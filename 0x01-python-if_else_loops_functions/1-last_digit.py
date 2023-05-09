01619740eaea54cec906d6da18af4974c61bca17
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-10 00:27:58 +0100

    task1c3

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 n_str = repr(number)
 n = int(n_str[-1])
+if number < 0:
+    n = - n
 if n > 5:
     str = "and is greater than 5"
 elif n == 0:
     str = "and is 0"
 else:
     str = "and is less than 6 and not 0"
-if number < 0:
-    n = - n
 print(f"Last figit of {number} is {n} {str}")
