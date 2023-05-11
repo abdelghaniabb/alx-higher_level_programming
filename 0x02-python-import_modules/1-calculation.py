b3ece649cb678592d100b8b672af39282f3e08cb
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-12 00:22:47 +0100

    task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import calculator_1 as ca
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, ca.add(a, b)))
+print("{} - {} = {}".format(a, b, ca.sub(a, b)))
+print("{} * {} = {}".format(a, b, ca.mul(a, b)))
+print("{} / {} = {}".format(a, b, ca.div(a, b)))
