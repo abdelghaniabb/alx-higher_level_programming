76de0c4dd831efae8e6e352102fee6ec07348255
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-12 10:35:22 +0100

    task2rat2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         if len(argv) == 2:
-            print("{} argument:".format(len(argv) - 1))
+            print("{:d} argument:".format(len(argv) - 1))
             for i in range(1, len(argv)):
-                print("{}: {}".format(i, argv[i]))
+                print("{:d}: {}".format(i, argv[i]))
         else:
-            print("{} arguments:".format(len(argv) - 1))
+            print("{:d} arguments:".format(len(argv) - 1))
             for i in range(1, len(argv)):
-                print("{}: {}".format(i, argv[i]))
+                print("{:d}: {}".format(i, argv[i]))
 
