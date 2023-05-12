35db9f5d997b53039c67d923a110a45f8f9b535d
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-12 10:12:54 +0100

    task1rat1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import calculator_1 as ca
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, ca.add(a, b)))
-print("{} - {} = {}".format(a, b, ca.sub(a, b)))
-print("{} * {} = {}".format(a, b, ca.mul(a, b)))
-print("{} / {} = {}".format(a, b, ca.div(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
