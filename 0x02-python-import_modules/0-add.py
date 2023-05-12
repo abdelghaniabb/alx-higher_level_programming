c8d47835f95bff74e1173b2088896b509bb83d95
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-12 10:09:07 +0100

    task0rat4

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,4 @@ import add_0 as s
 if __name__ == "__main__":
     a = 1
     b = 2
-    r = s.add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, r))
+    print("{:d} + {:d} = {:d}".format(a, b, s.add(a, b)))
