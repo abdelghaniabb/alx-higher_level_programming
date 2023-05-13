228eca9288b682a6d1255ea4312a0550da13277d
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-13 23:36:15 +0100

    task0-c2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,5 @@
 def replace_in_list(my_list, idx, element):
     if my_list:
         if idx >= 0 and idx < len(my_list):
-            my_list[idx] = element
+            my_list.insert(idx, element)
         return my_list
