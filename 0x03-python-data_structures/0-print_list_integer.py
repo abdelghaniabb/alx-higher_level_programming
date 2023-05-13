b79229f068a81382cb12e6dbcd47dff341d50b26
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-13 22:16:16 +0100

    task0-x7

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if not my_list:
-        exit()
-    for i in reversed(my_list):
+    if my_list is None:
+        my_list = []
+    for i in my_list:
         print("{:d}".format(i))
