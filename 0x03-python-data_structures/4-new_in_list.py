862186694b2c8535085d6a809825047d8a139bcb
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-14 19:51:56 +0100

    ask5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx >= 0 and idx < len(my_list):
+        for i in my_list:
+            new_list.append(i)
+        new_list[idx] = element
+        return new_list
