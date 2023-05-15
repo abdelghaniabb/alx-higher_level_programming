57cb37f052256b80e6bc861dc2f17261178e0a62
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-15 15:58:30 +0100

    atsk8-c3

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,8 @@ def multiple_returns(sentence):
     if sentence is None:
         sentence = ""
     lenght = len(sentence)
-    first_char = sentence[0]
     if lenght == 0:
         first_char = None
+    else:
+        first_char = sentence[0]
     return (lenght, first_char)
