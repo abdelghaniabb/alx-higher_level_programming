9f49f51e165594492903a2342432429158f4c7a8
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-15 16:35:07 +0100

    task11-c1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,5 @@ def delete_at(my_list=[], idx=0):
     if my_list is None:
         my_list = []
     if idx >= 0:
-        for i in range(idx, len(my_list) - 1):
-                my_list[i] = my_list[i + 1]
-    del my_list[i + 1]
+        del my_list[idx]
     return my_list
