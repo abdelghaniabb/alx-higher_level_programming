a492e38645afbe16179eb2f6e5a13e35a323869c
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-15 13:14:17 +0100

    task7-c1

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-def check(tuple_=(), i):
-    if len(tuple_) >= i + 1:
-        a = tuple_[i]
-    else:
+def check(i, tuple_=None):
+    if tuple_ is None or len(tuple_) < i + 1:
         a = 0
+    else:
+        a = tuple_[i]
     return a
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1 = check(tuple_a, 0)
-    a2 = check(tuple_a, 1)
-    b1 = check(tuple_b, 0)
-    b2 = check(tuple_b, 1)
+    if tuple_a is None or len(tuple_a) < 1:
+        a1 = 0
+    else:
+        a1 = tuple_a[0]
+    if tuple_a is None or len(tuple_a) < 2:
+        a2 = 0
+    else:
+        a2 = tuple_a[1]
+    if tuple_b is None or len(tuple_b) < 1:
+        b1 = 0
+    else:
+        b1 = tuple_b[0]
+    if tuple_b is None or len(tuple_b) < 2:
+        b2 = 0
+    else:
+        b2 = tuple_b[1]
     return (a1 + b1, a2 + b2)
-print(add_tuple((1, 2), (1,2)))    
