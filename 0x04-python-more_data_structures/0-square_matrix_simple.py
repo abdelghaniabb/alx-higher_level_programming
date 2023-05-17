e6cc6d7e5c8ab19d80b9417606ad11c125d09e28
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-17 15:13:35 +0100

    task0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        matrix = []
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for i in range(0, len(row)):
+            new_row.append(row[i] * row[i])
+        new_matrix.append(new_row)
+    return new_matrix
