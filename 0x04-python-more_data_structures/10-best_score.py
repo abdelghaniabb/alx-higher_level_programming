b92fae0e2e9bdd6defc2c1148b8ae152da593b25
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-18 00:16:35 +0100

    task10-c2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     best_key = None
     score = -100
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for key in a_dictionary:
             if a_dictionary[key] > score:
                 score = a_dictionary[key]
