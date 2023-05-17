295a3299da7406d4daf7e6fbda93a82e0a5e9e16
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-17 21:13:40 +0100

    task3-c1

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
+    found_elements = []
     for ele1 in set_1:
         for ele2 in set_2:
-            if ele1 ==  ele2:
-                common.append(ele1)
-    return common
+            if ele1 == ele2:
+                found_elements.append(ele1)
+    return found_elements
