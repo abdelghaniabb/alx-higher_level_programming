b9f7632cb061dc299c04aed89685cae705a7aefd
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-31 10:42:23 +0100

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    count = 0
+    while i + 1 <= x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i = i + 1
+            count = count + 1
+        except Exception:
+            i = i + 1
+    print("")
+    return count
