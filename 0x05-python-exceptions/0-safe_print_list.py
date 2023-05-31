901539a803e09d09aebbfb27c995b5b9247ea0a5
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-31 10:18:39 +0100

    task0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        while count + 1 <= x:
+            print("{}".format(my_list[count]), end="")
+            count = count + 1
+    except:
+        print("")
+        return count
+    print("")
+    return count
+
