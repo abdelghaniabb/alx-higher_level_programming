640c4b8dbdbd790393e7cd224f6faa4baa36f991
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-08 00:43:26 +0100

    task10-v3

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include "lists.h"
+
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
  * @list: pointer to list to be freed
  * Return: int
  */
- int check_cycle(listint_t *list)
- {
+int check_cycle(listint_t *list)
+{
 	listint_t *current;
- 
+
 	if (list == NULL)
 		return (0);
 
@@ -20,4 +21,4 @@
 		current = current->next;
 	}
 	return (0);
- }
+}
