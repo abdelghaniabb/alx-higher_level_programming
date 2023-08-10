8a891be93ba20bbf975e81d753582a95a9408f56
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-10 23:21:13 +0100

    tak3-v1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-s = 0
-if len(sys.argv) > 1:
-    for i in range(1, len(sys.argv)):
-        s = s + int(sys.argv[i])
-print("{}".format(s)) 
+if __name__ = "__main__":
+    s = 0
+    if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            s = s + int(sys.argv[i])
+    print("{}".format(s)) 
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,3 +1,4 @@
 # 0x02. Python - import & modules
+## 2-args.py
 ## 4-hidden_discovery.py
 ## 100-my_calculator.py
