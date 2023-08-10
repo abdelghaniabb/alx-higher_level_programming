c5aa94b4152cc2afcca389e4fe0253f5f50aa689
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-10 23:22:22 +0100

    task3-3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
-if __name__ = "__main__":
+if __name__ == "__main__":
     s = 0
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             s = s + int(sys.argv[i])
-    print("{}".format(s)) 
+    print("{:d}".format(s)) 
