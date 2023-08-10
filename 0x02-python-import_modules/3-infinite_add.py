88715adda3290e5cc58694e3dd8f89080b70885c
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-10 23:23:06 +0100

    task3-4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             s = s + int(sys.argv[i])
-    print("{:d}".format(s)) 
+    print("{:d}".format(s))
