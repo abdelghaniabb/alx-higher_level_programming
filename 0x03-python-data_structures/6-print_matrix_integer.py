56b9bfda5277fbf64c6a7ba76daa96bda10794a1
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-12 19:42:50 +0100

    task6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+        return
+    for line in matrix:
+        for i in range(0, len(line) - 1):
+            print("{} ".format(line[i]), end="")
+        print("{}".format(line[len(line) - 1])) 
