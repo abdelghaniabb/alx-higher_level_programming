9c90bc03b42c4f11aa8558ced78d33061f7e3e62
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-15 10:11:20 +0100

    task4-done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     names = []
     for name in dir(hidden_4):
         if not name.startswith('__'):
-            names.append(name)
-        names.sort()
-        for name in names:
-            print(name)
+            if not (name in names):
+                names.append(name)
+    names.sort()
+    for name in names:
+        print(name)
