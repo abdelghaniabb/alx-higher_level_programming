c15cc874a4ff866f48e33e07634a7f9656213294
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-18 00:21:08 +0100

    task13-rat

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if len(my_list) == 0:
         return 0
-    s = 0
-    for item in my_list:
-        s = s + item[1]
-    return s / len(my_list)
+
+    total_weighted_sum = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_weighted_sum += score * weight
+        total_weight += weight
+
+    if total_weight == 0:
+        return 0
+
+    weighted_average = total_weighted_sum / total_weight
+    return weighted_average
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,2 +1,7 @@
 # 0x04. Python - More Data Structures: Set, Dictionary
 - 12-roman_to_int.py
+- 100-weight_average.py
+
+## understinding
+- stored dictioneray
+- map function for list
