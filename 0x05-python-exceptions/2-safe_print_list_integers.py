5efa49971e235f94924afb82b027e2eaf7c92dfb
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-29 10:21:46 +0100

    gds

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,6 +11,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except Exception:
             i = i + 1
             continue
-    print("{:d}".format(my_list[i]), end="")
+    print("{:d}".format(my_list[i - 1]), end="")
     print("")
     return count
