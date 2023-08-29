d1c931e55712cfd7a709974214f91870e141cde8
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-29 10:28:56 +0100

    task4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,15 +13,16 @@ def list_division(my_list1, my_list2, list_length):
                 v2 = int(my_list2[index])
                 if v2 == 0:
                     print("division by 0")
-                    result = 0
                     index = index + 1
             except Exception:
                 if len(my_list1) <= index + 1 or len(my_list2) <= index + 1:
                     print("out of range")
                 else:
                     print("wrong type")
-                result = 0
                 index = index + 1
+            finally:
+                result = 0
+
         new.append(result)
 
     return new
