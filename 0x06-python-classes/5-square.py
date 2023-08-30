8982b3e3fb99635782568a92ff96f5e213a26b6a
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-30 17:27:08 +0100

    t25

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""5-square.py"""
+
+
+class Square:
+    """
+        This class defines a square and includes methods to
+        get and set its size, calculate its area and print it.
+        Attributes:
+            __size (int): The size of the square (private attribute).
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+        Args:
+            size (int): The size of the square.
+        Returns:
+            None
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            Getter method to retrieve the size of the square
+            Returns:
+                int: the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+            Setter method to set the size of the square
+            Args:
+                value (int): The size of the square.
+            Raises:
+                TypeError: size is not integer
+                ValueError: Size < 0
+            Returns:
+                None
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+            Calculates and returns the area of the square.
+            Returns:
+                int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+            prints the square with the character #
+            Returns:
+                None
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            i = 0
+            while i < self.__size:
+                j = 0
+                while j < self.__size:
+                    print("#", end="")
+                    j = j + 1
+                print("")
+                i = i + 1
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,4 +1,3 @@
 # 0x06. Python - Classes and Objects
 - https://www.py4e.com/lessons/Objects
 
-- 2-square.py
