a4224eb3be642f437545a30e66b2261eaa4731fe
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-30 12:53:21 +0100

    t3

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-# 0-square.py
+""" 0-square.py"""
 class Square:
     """
     This is an empty class to demonstrate docstring usage.
-
-    This class doesn't have any methods or attributes defined within it.
-    It serves as a template for future implementations.
-
-    Attributes:
-        None
-
-    Methods:
-        None
     """
     pass
