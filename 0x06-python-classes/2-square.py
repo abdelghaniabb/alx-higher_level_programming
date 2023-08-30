af74a731a3de5f56764f8ae0bbee0f9d5d13b1a3
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-30 16:19:07 +0100

    t21

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,11 @@
 
 
 class Square:
-    """ square class """
+    """
+        This class defines a square by its size.
+        Attributes:
+            __size (int): The size of the square (private attribute).
+    """
     def __init__(self, size=0):
         """Constructor
             Args:
@@ -13,7 +17,7 @@ class Square:
                     TypeError: size is not integer
                     ValueError: Size < 0
         """
-        if not(type(size) == int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
