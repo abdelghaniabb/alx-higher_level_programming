fc2ba6aa78357331d1972ea5b6fee79a41019bb6
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-07 14:43:19 +0100

    r2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+""" add integers"""
+
+
 def add_integer(a, b=98):
+    """
+        this function adds tow integers
+        Args:
+            a: first number
+            b: second number
+        Raises:
+            TypeError: the numbers must be integers
+    """
     if not(isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an intger")
     if not(isinstance(b, int) or isinstance(b, float)):
