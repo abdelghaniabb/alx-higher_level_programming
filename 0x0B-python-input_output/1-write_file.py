f5c1c92d55592c277708ac43f9f1c88a3baf406e
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-12 13:56:40 +0100

    task1-1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,14 @@
 
 
 def write_file(filename="", text=""):
+    """
+        writes a string to a text file
+        Args:
+            filename: the name of the file
+            text: the text to add to the file
+        Returns:
+            number of char that added to the fole
+    """
     with open(filename, "w", encoding="utf-8") as outfile:
         i = 0
         for char in text:
