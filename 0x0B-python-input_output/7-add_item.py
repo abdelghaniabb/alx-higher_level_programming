1bae41958447944eaf594ef81560a49785e3d966
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-12 22:08:40 +0100

    u

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,13 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     my_list = load_from_json_file("add_item.json")
     if not my_list:
         my_list = []
 
-
     for i in range(1, len(sys.argv)):
         my_list.append(sys.argv[i])
 
-    save_to_json_file(my_list, "add_item.json")
+    load_json_file(my_list, "add_item.json")
