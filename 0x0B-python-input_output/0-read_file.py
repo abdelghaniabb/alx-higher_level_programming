7de2bf6be56806dbf42634fb59ff8124fc37be04
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-12 12:53:57 +0100

    task0_1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""read a text file"""
+
+
 def read_file(filename=""):
+    """read a text file and prints it to stdout"""
     with open(filename, "r") as infile:
         print(infile.read())
