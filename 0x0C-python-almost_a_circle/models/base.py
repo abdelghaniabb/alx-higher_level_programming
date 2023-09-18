e37480eb578ede17558b464344fb54e30d9e9c03
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-18 15:45:44 +0000

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,8 +3,21 @@
 
 
 class Base():
+    """
+        This class will be the “base” of all other classes in this project.
+        Attributes:
+            __nb_objects (int): A private class attribute to keep track of the number of instances created.
+    """
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """
+            Initialize a new instance of Base
+            Args:
+                id: the ID to assign to the instance
+            Returns:
+                None
+        """
         if id is not None:
             self.id = id
         else:
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+        defines a rectangle
+        Attributes:
+            __width: the width of the rectangle
+            __height: the height of the rectangle
+            __x: the horizontal position of the rectangle
+            __y: the vertical position of the rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            initializes a Rectangle instance
+            Args:
+                width: the width of the rectangle
+                height: the height of the rectangle
+                x: the horizontal position of the rectangle
+                y: the vertical position of the rectangle
+                id: the ID of the rectangle
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """returns the area value of the Rectangle instance"""
+        return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        for i in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for j in range(self.__x):
+                print(" ", end="")
+            print("#" * self.__width)
+
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        a, b, c = self.id, self.__x, self.__y
+        e, f = self.__width, self.__height
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(a, b, c, e, f)
+
+    def update(self, *args):
+        """ assigns an argument to each attribute"""
+        for key, val in ["id", "width", "heigh", "x", "y"], argv:
+            self.__"{}".format(key) = val
+
+        self.id = args[0]
+        self.__width = args[1]
+        self.__height = args[2]
+        self.__x = args[3]
+        self.__y = args[4]
