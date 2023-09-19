040ee6d767343d58d451ec987d65de79cbc189d8
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-19 20:45:12 +0100

    fdj

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,3 +27,16 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+            Returns JSON string representation of a list of dictionaries.
+            Args:
+                list_dictionaries: A list of dictionaries
+            Returns:
+                str: JSON string representation of the list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
