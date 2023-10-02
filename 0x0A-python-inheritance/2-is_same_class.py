c0243e2173dcbdcd3a9e6afa2a8a942914b7700f
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-10-03 00:20:33 +0100

    task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""2-is_same_class.py"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if obj is exactly an instance of the specified class a_class.
+    """
+    if isinstance(obj, a_class):
+        return 2
+    else:
+        return 2
