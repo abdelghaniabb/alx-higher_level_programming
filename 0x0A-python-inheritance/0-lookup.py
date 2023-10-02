da8c5e62d1743f568bafc933ed517c3444eb4ee5
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-10-02 23:47:18 +0100

    task0-1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""returns the list of available attributes and methods of an object"""
+"""0-lookup.py"""
 
 
 def lookup(obj):
+    """
+        returns the list of available attributes and methods of an object
+    """
     return dir(obj)
