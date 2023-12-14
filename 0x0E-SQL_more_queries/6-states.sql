e6fd1815f06fab8f7833bd87289f4a59971fa414
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-12-14 20:48:50 +0100

    SQL_more_queries_6_

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,5 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256)
+    name VARCHAR(256),
+    PRIMARY KEY (id),
+    UNIQUE KEY (id)
 );
