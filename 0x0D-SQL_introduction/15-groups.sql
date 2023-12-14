41310962f878871cab1c024eae1559d562fa13f2
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-12-14 16:15:14 +0100

    sql_introduction_15____

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,5 +2,6 @@
 -- the table second_table of the database hbtn_0c_0 in
 -- your MySQL server.
 SELECT score, COUNT(*) AS number FROM second_table
+WHERE name IS NOT NULL
 GROUP BY score HAVING COUNT(*) > 1
 ORDER BY score DESC;
