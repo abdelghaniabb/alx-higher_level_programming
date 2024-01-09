a61162f1c81f4cfe04f6905d3e288f8fcfee733d
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2024-01-08 17:26:11 -0800

    task9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,11 +2,10 @@
 
 const first = parseInt(process.argv[2]);
 const second = parseInt(process.argv[3]);
-let result;
 
-function add(a, b) {
+function add (a, b) {
   return (a + b);
 }
 
-result = add(first, second);
+const result = add(first, second);
 console.log(result);
