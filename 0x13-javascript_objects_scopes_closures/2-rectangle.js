f2b25f9a56e5f1f0bf2d311e132d5e7d8e4dd391
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2024-01-09 16:32:51 -0800

    task2-3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
 
-class Rectangle {
-  width;
-  height;
-
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      return {};
     }
   }
-}
-
-module.exports = Rectangle;
+};
