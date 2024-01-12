7720681aa6365124104ad2bb774340a4f527e534
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2024-01-12 02:44:09 -0800

    task6-1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,21 +2,22 @@
 
 const s5 = require('./5-square');
 
-class Square extends s5 {
-	constructor (size) {
-		super(size);
-	}
+module.exports = class Square extends s5 {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
 
-	charPrint (str) {
-		if (!str) {
-			str = 'X';
-			for (let i = 0; i < this.size; i++) {
-				row = '';
-				for (let j = 0; j < this.size; j++) {
-					row += str;
-				}
-				console.log(str);
-			}
-		}
-	}
+  charPrint (str) {
+    if (!str) {
+      str = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        row += str;
+      }
+      console.log(row);
+    }
+  }
 };
