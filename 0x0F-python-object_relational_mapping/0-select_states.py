cd83ef42b74eb58ab5d50bcdabc6f2ac8e9cce43
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2024-01-12 21:23:43 +0100

    task0-1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,17 +5,17 @@
 import MySQLdb
 import sys
 
-#Trying to connect 
+# Trying to connect
 try:
-    db_connection= MySQLdb.connect(
+    db_connection = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3])
 # If connection is not successful
-except:
-    print("Can't connect to database")
+except Exception as e:
+    print(e)
     exit()
 
 cursor = db_connection.cursor()
@@ -25,6 +25,6 @@ data = cursor.fetchall()
 
 for state in data:
     print(state)
-    
+
 cursor.close()
 db_connection.close()
