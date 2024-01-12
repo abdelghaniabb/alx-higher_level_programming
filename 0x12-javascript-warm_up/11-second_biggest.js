ecc60c87a1ec5d71680365afd25516f32579fe01
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2024-01-11 17:14:18 -0800

    task11-2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,14 +5,14 @@ const len = process.argv.length;
 if (len <= 3) {
   console.log(0);
 } else {
-  let firstBiggest = process.argv[2];
-  let secondBiggest = process.argv[2];
+  let firstBiggest = parseInt(process.argv[2]);
+  let secondBiggest = parseInt(process.argv[2]);
   for (let i = 3; i < len; i++) {
-    if (firstBiggest < process.argv[i]) {
+    if (firstBiggest < parseInt(process.argv[i])) {
       secondBiggest = firstBiggest;
-      firstBiggest = process.argv[i];
-    } else if (secondBiggest < process.argv[i]) {
-      secondBiggest = process.argv[i];
+      firstBiggest = parseInt(process.argv[i]);
+    } else if (secondBiggest < parseInt(process.argv[i])) {
+      secondBiggest = parseInt(process.argv[i]);
     }
   }
   console.log(secondBiggest);
