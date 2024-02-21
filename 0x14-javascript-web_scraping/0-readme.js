8df67afc17338541163a53500ac38b16252712b9
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2024-02-21 15:19:08 -0800

    task0-2

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,18 +2,16 @@
 const fs = require('fs');
 
 if (process.argv.length !== 3) {
-	console.error('Usage: node 0-readme.js fileName');
-	process.exit(1);
+  console.error('Usage: node 0-readme.js fileName');
+  process.exit(1);
 }
 
 const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf-8', (error, data) => {
-	if (error) {
-		console.error(error);
-	} else {
-		console.log(data);
-	}
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
 });
-
-
